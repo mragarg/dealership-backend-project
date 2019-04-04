a0b5f7aba5cced4240ec65dec56b5bfc62e83d16
Ashish Garg <mragarg@gmail.com>
2019-04-04 09:32:02 -0400

    Adds design of tables for car dealer app

---
+++ schema.sql
@@ -0,0 +1,22 @@
+create table users {
+    id serial primary key,
+    first_name varchar(100),
+    last_name varchar(100),
+    email varchar(200),
+    password varchar(500)
+};
+
+create table cars {
+    id serial primary key,
+    year integer,
+    make varchar(100),
+    model varchar(100),
+    price integer,
+    mileage integer
+};
+
+create table sold-cars {
+    id serial primary key,
+    car_id integer references cars(id),
+    user_id integer references users(id)
+}
